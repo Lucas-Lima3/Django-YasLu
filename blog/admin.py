9ee16c28247904b3d275d94b2cc2288711b0896c
Yasmin Storti <yasmin.y_s@hotmail.com>
2021-05-13 12:32:09 -0300

    Adicionar pasta admin com models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from blog.models import Publication
 
-# Register your models here.
+
+class PublicationAdmin(admin.ModelAdmin):
+    date_hierarchy = 'publication_date'
+    list_display = ('nome', 'cwid', 'publication_date')
+    list_filter = ('nome', 'cwid')
+    search_fields = ('nome', 'cwid')
+
+
+admin.site.register(Publication, PublicationAdmin)
+
