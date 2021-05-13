ba608bcb7f95a2bd548662c94bc3973a053aeff5
Yasmin Storti <yasmin.y_s@hotmail.com>
2021-05-13 12:36:29 -0300

    Criar pasta de views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Publication
+
+
+def render_index(request):
+    return render(request, 'index.html', {})
+
+
+def render_publication_done(request):
+    publication_data = {
+        'nome': request.POST.get('nome'),
+        'cwid': request.POST.get('cwid'),
+        'opicoes_bayflex': request.POST.get('opicoes_bayflex'),
+        'email': request.POST.get('email'),
+    }
+    publication = Publication(**publication_data)
+    publication.save()
+    return render(request, 'publication_done.html', {'nome': publication.nome})
