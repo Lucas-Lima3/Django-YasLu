a9b6f9107590b553cf43724be396f503a00eb406
Yasmin Storti <yasmin.y_s@hotmail.com>
2021-05-13 20:19:24 -0300

    Criar css e index

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Publication(models.Model):
-	nome = models.CharFiels(max_length=200)
+	nome = models.CharField(max_length=200)
 	cwid = models.CharField(max_length=10)
-	opicoes_bayflex = models.TextFields()
-	email = models.TextFields()
-	publication_date = models.DateTimeFiels(auto_now_add=True)
-	update_at = models.DateTimeFiels(auto_now_add=True)
+	opicoes_bayflex = models.TextField()
+	email = models.TextField()
+	publication_date = models.DateTimeField(auto_now_add=True)
+	update_at = models.DateTimeField(auto_now_add=True)
 
