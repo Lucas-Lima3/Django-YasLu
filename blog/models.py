df61ff08e9fd3f18c50a148586382edab08b6e7f
Yasmin Storti <yasmin.y_s@hotmail.com>
2021-05-13 12:21:26 -0300

    Adicionar publicação model para o app blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Publication(models.Model):
+	nome = models.CharFiels(max_length=200)
+	cwid = models.CharField(max_length=10)
+	opicoes_bayflex = models.TextFields()
+	email = models.TextFields()
+	publication_date = models.DateTimeFiels(auto_now_add=True)
+	update_at = models.DateTimeFiels(auto_now_add=True)
+
